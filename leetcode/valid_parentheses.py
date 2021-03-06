c2b7130d17cc97f6296c9fc273c4a1392816874c
masa08 <ushimasarug08@gmail.com>
2021-03-06 11:20:58 +0900

    :sparkles: Add valid parentheses

---
+++ leetcode/valid_parentheses.py
@@ -0,0 +1,24 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+
+
+# stackにpushとpopを繰り返して、parenthesesの整合性を確かめる
+# 最終的にstackが空になれば整合性が取れたことになるので、not stackがtrueの場合に成功
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
