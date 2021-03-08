bf3a7db30cc8ec488bd3a51176313f0bed875720
masa08 <ushimasarug08@gmail.com>
2021-03-09 08:20:18 +0900

    :sparkles: Add move zeros

---
+++ leetcode/move_zeros.py
@@ -0,0 +1,17 @@
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# numsに0が存在した場合、jの位置とi(0が存在する)の位置を入れ替え続けることで、0を後方に持っていく。
+
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
