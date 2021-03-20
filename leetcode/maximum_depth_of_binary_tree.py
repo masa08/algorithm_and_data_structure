41f10bfb072a0e9aeecdbaa4abe3d4ecfe68c462
masa08 <ushimasarug08@gmail.com>
2021-03-21 08:55:49 +0900

    :sparkles: Add maximum depth of binary tree

---
+++ leetcode/maximum_depth_of_binary_tree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# 再帰的に呼び出してmaxを比較するのでnullの場合も考慮される。
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
