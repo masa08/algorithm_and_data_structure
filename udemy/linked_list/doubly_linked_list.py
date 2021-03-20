35c68a2bd508a32fd991865f79c1c7046ada4e01
masa08 <ushimasarug08@gmail.com>
2021-03-20 10:07:03 +0900

    :sparkles: Add double linked list reverse

--- udemy/linked_list/doubly_linked_list.py
+++ udemy/linked_list/doubly_linked_list.py
@@ -73,6 +73,35 @@ class DoublyLinkedList(object):
             current_node = None
             return
 
+    def reverse_iterative(self) -> None:
+        previous_node = None
+        current_node = self.head
+        while current_node:
+            previous_node = current_node.prev
+            current_node.prev = current_node.next
+            current_node.next = previous_node
+
+            current_node = current_node.prev
+
+        if previous_node:
+            self.head = previous_node.prev
+
+    def reverse_recursive(self) -> None:
+        def _reverse_recursive(current_node: Node) -> Optional[Node]:
+            if not current_node:
+                return None
+
+            previous_node = current_node.prev
+            current_node.prev = current_node.next
+            current_node.next = previous_node
+
+            if current_node.prev is None:
+                return current_node
+
+            return _reverse_recursive(current_node.prev)
+
+        self.head = _reverse_recursive(self.head)
+
 
 if __name__ == '__main__':
     d = DoublyLinkedList()
@@ -81,6 +110,6 @@ if __name__ == '__main__':
     d.append(3)
     d.insert(0)
     d.print()
-    print("######## Remove")
-    d.remove(3)
+    print("######## Reverse")
+    d.reverse_recursive()
     d.print()
