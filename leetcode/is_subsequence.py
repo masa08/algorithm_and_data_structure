200c32259dd69b93828c2e32fe90eb59c1459aa1
masa08 <ushimasarug08@gmail.com>
2021-03-30 09:00:59 +0900

    :sparkles: Add is subsequence another

--- leetcode/is_subsequence.py
+++ leetcode/is_subsequence.py
@@ -14,3 +14,19 @@ class Solution:
             return True
         else:
             return False
+
+
+# tの中にあるs文字のindexを取得して、tをindex以降の文字列に変換する。これを最後まで繰り返す。
+# もし、tのなかにsが存在しない場合は、sの並び順を満たさないことになるので例外、return Falseが返却される。
+# 問題なく処理を終了した場合、is_subsequenceなことが証明されるのでreturn Trueを返す。
+# class Solution:
+# 	def isSubsequence(self, s: str, t: str) -> bool:
+# 		for i in range(0, len(s)):
+# 			try:
+# 				index = t.index(s[i])
+# 			except ValueError:
+# 				return False
+
+# 			t = t[index+1:]
+
+# 		return True
