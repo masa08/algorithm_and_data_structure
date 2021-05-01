f30cfafeb7c872daff068d0b6e6ad91704dc45dc
masa08 <ushimasarug08@gmail.com>
2021-05-01 13:23:27 +0900

    :sparkles: Add Diameter of Binary Tree

---
+++ leetcode/easy/diameter_of_binary_tree.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def path_length(self, root: TreeNode):
+        if root is None:
+            return 0
+        left_path = self.path_length(root.left)
+        right_path = self.path_length(root.right)
+        path = left_path + right_path
+        if path > self.diameter:
+            self.diameter = path
+
+        return max(left_path, right_path) + 1
+
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.diameter = 0
+        self.path_length(root)
+        return self.diameter
