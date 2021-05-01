7f6898af37ba50c0c70d229a3e7e53b1b8e9cc5e
masa08 <ushimasarug08@gmail.com>
2021-05-01 12:53:31 +0900

    :sparkles: Update Find All Numbers Disappeared in an Array

--- leetcode/easy/find_all_numbers_disappeared_in_an_array.py
+++ leetcode/easy/find_all_numbers_disappeared_in_an_array.py
@@ -3,7 +3,17 @@ from typing import List
 
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        rangeList = [i for i in range(1, len(nums)+1)]
-        result = set(rangeList) - set(nums)
+        # 1..nまででnumsに存在する部分は全て負の値にする
+        for num in nums:
+            ans = abs(num) - 1
+            if nums[ans] > 0:
+                nums[ans] *= -1
 
-        return result
+        # 負の値でない部分を返す
+        return [i+1 for i in range(len(nums)) if nums[i] > 0]
+# class Solution:
+#     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+#         rangeList = [i for i in range(1, len(nums)+1)]
+#         result = set(rangeList) - set(nums)
+
+#         return result
