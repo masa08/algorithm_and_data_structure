a9b1c4e2bfdeea3d18911ee42cfb6182969a0276
masa08 <ushimasarug08@gmail.com>
2021-06-02 09:26:32 +0900

    :sparkles: Add squeares sorted array

---
+++ leetcode/easy/squeres_sorted_array.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    def sortedSquares1(self, nums: List[int]) -> List[int]:
+        result = []
+        for n in nums:
+            result.append(n**2)
+
+        return sorted(result)
+
+    def sortedSquares2(self, A: List[int]) -> List[int]:
+        result = [0] * len(A)
+        left, right = 0, len(A) - 1
+        for index in range(len(A)-1, -1, -1):
+            if abs(A[left]) > abs(A[right]):
+                result[index] = A[left] ** 2
+                left += 1
+            else:
+                result[index] = A[right] ** 2
+                right -= 1
+        return result
+
+    def sortedSquares3(self, A: List[int]) -> List[int]:
+        return_array = [0] * len(A)
+        write_pointer = len(A) - 1
+        left_read_pointer = 0
+        right_read_pointer = len(A) - 1
+        left_square = A[left_read_pointer] ** 2
+        right_square = A[right_read_pointer] ** 2
+        while write_pointer >= 0:
+            if left_square > right_square:
+                return_array[write_pointer] = left_square
+                left_read_pointer += 1
+                left_square = A[left_read_pointer] ** 2
+            else:
+                return_array[write_pointer] = right_square
+                right_read_pointer -= 1
+                right_square = A[right_read_pointer] ** 2
+            write_pointer -= 1
+        return return_array
