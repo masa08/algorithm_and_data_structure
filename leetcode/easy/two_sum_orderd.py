f8409ce30d1ddf254fa2749b217a18c4c71d2555
masa08 <ushimasarug08@gmail.com>
2021-06-04 09:25:40 +0900

    :sparkles: Add two sum orderd

---
+++ leetcode/easy/two_sum_orderd.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # cache
+        # seen = {}
+        # for i, v in enumerate(numbers):
+        #     remaining = target - v
+        #     if remaining in seen:
+        #         return [seen[remaining], i + 1]
+        #     seen[v] = i + 1
+        # return []
+
+        # two pointer
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            if numbers[l] + numbers[r] == target:
+                return [l+1, r+1]
+            elif numbers[l] + numbers[r] > target:
+                r -= 1
+            else:
+                l += 1
+        return []
