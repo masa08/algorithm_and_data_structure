039a4bdaab7ad98e1a1b4b559b6fe60932e38547
masa08 <ushimasarug08@gmail.com>
2021-06-10 09:17:13 +0900

    :sparkles: Update range sum of bst

--- leetcode/easy/range_sum_of_bst.py
+++ leetcode/easy/range_sum_of_bst.py
@@ -4,9 +4,16 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-
-
 class Solution:
+    def rangeSumBSTFast(self, root: TreeNode, L: int, R: int) -> int:
+        if root is None: return 0
+        # 対象範囲ではないvalの場合は次の処理に移る
+        if root.val > R: return self.rangeSumBST(root, L, R)
+        if root.val < L: return self.rangeSumBST(root, L, R)
+        # 対象範囲のvalであればvalを回帰的に足し合わせてreturnする
+        return root.val + self.rangeSumBST(root.left,L,R) + self.rangeSumBST(root.right,L,R)
+
+
     def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
         target_num = []
 
