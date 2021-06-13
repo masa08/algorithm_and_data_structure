8f6fdd7d6468b76929a643b15994bb20e51cffdd
masa08 <ushimasarug08@gmail.com>
2021-06-13 12:28:01 +0900

    :sparkles: Add Sum of Left Leaves

---
+++ leetcode/easy/sum_of_left_leaves.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        self.total = 0
+
+        def _sumOfLeftLeaves(root):
+            if not root.left and not root.right:
+                return
+            if root.left:
+                if root.left.left is None and root.left.right is None:
+                    self.total += root.left.val
+                _sumOfLeftLeaves(root.left)
+            if root.right:
+                _sumOfLeftLeaves(root.right)
+
+        _sumOfLeftLeaves(root)
+        return self.total
