eb023b17bd79bce7a192e1697d183b62700fbf11
masa08 <ushimasarug08@gmail.com>
2021-06-15 09:16:25 +0900

    :sparkles: Add edpc a

---
+++ atcoder/edpc/a.py
@@ -0,0 +1,14 @@
+def main():
+    N = int(input())
+    h = list(map(int, input().split()))
+
+    DP = [0]*(N)
+    DP[1] = abs(h[1]-h[0])
+
+    for i in range(2, N):
+        DP[i] = min(abs(h[i]-h[i-2])+DP[i-2], abs(h[i]-h[i-1])+DP[i-1])
+
+    return DP[N-1]
+
+
+print(main())
