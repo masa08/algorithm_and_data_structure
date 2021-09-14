167eb5869bbf2bae2826dceb61b4687d4d5856eb
masa08 <ushimasarug08@gmail.com>
2021-09-14 09:23:13 +0900

    :sparkles: add reverse string

---
+++ leetcode/easy/reverse_string.ts
@@ -0,0 +1,15 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+  let left: number = 0;
+  let right: number = s.length - 1;
+
+  while (left <= right) {
+    let temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }
+}
