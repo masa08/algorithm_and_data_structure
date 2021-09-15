ab880b4b6cc137c27d1b685b0deed42b196dbb80
masa08 <ushimasarug08@gmail.com>
2021-09-15 22:29:12 +0900

    :sparkles: update intersection

--- leetcode/easy/intersection_of_two_linked_lists_two.py
+++ leetcode/easy/intersection_of_two_linked_lists_two.py
@@ -13,24 +13,20 @@ class Solution:
         return result
 
 # two pointer
-# class Solution(object):
-#     def intersect(self, nums1, nums2):
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
+        nums1, nums2 = sorted(nums1), sorted(nums2)
+        pt1 = pt2 = 0
 
-#         nums1, nums2 = sorted(nums1), sorted(nums2)
-#         pt1 = pt2 = 0
-#         res = []
+        while pt1 < len(nums1) and pt2 < len(nums2):
+            if nums1[pt1] == nums2[pt2]:
+                res.append(nums1[pt1])
+                pt1 += 1
+                pt2 += 1
+            elif nums1[pt1] < nums2[pt2]:
+                pt1 += 1
+            else:
+                pt2 += 1
 
-#         while True:
-#             try:
-#                 if nums1[pt1] > nums2[pt2]:
-#                     pt2 += 1
-#                 elif nums1[pt1] < nums2[pt2]:
-#                     pt1 += 1
-#                 else:
-#                     res.append(nums1[pt1])
-#                     pt1 += 1
-#                     pt2 += 1
-#             except IndexError:
-#                 break
-
-#         return res
+        return res
