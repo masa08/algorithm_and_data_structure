ef51200378d733ce42b3f3432d6bb681713fcce9
masa08 <ushimasarug08@gmail.com>
2021-09-16 09:41:41 +0900

    :sparkles: add pascal ts

---
+++ leetcode/easy/pascals_triangle.ts
@@ -0,0 +1,18 @@
+function generate(numRows: number): number[][] {
+  const dp: number[][] = Array(numRows)
+    .fill(null)
+    .map((_, i) => Array(i + 1));
+
+  for (let i = 0; i < dp.length; i++) {
+    dp[i][0] = 1;
+    dp[i][dp[i].length - 1] = 1;
+  }
+
+  for (let i = 2; i < numRows; i++) {
+    for (let j = 1; j < i; j++) {
+      dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+    }
+  }
+
+  return dp;
+}
