9e031f37288828874b843d62a76bee0b0341e011
masa08 <ushimasarug08@gmail.com>
2021-09-16 10:25:53 +0900

    :sparkles: update sort

--- udemy/sort/quick_sort.py
+++ udemy/sort/quick_sort.py
@@ -1,14 +1,15 @@
-# pivodより小さい数、大きい数を整理して、再帰的に繰り返すことによりソートを実行する。
+# pivotより小さい数、大きい数を整理して、再帰的に繰り返すことによりソートを実行する。
 from typing import List
 
 
 def partition(numbers: List[int], low: int, high: int) -> int:
     i = low - 1
-    pivod = numbers[high]
+    pivot = numbers[high]
     for j in range(low, high):
-        if numbers[j] <= pivod:
+        if numbers[j] <= pivot:
             i = i + 1
             numbers[i], numbers[j] = numbers[j], numbers[i]
+    # i+1以下はnumbers[high]よりも小さく、i+1以上はnumbers[high]より大きい
     numbers[i+1], numbers[high] = numbers[high], numbers[i+1]
     return i+1
 
@@ -17,7 +18,9 @@ def quick_sort(numbers: List[int]) -> List[int]:
     def _quick_sort(numbers: List[int], low: int, high: int) -> None:
         if low < high:
             partition_index = partition(numbers, low, high)
+            # pivotの左側
             _quick_sort(numbers, low, partition_index - 1)
+            # pivotの右側
             _quick_sort(numbers, partition_index + 1, high)
 
     _quick_sort(numbers, 0, len(numbers) - 1)
