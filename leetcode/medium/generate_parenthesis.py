e9514ee0b71abce46cb41afd9725be0ff9165550
masa08 <ushimasarug08@gmail.com>
2021-09-27 09:33:09 +0900

    :sparkles: add generate parentheses

---
+++ leetcode/medium/generate_parenthesis.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def backtrack(ans, curr, openp, closep, maxp):
+            if len(curr) == 2*maxp:
+                ans.append(curr)
+            if openp < maxp:
+                backtrack(ans, curr+'(', openp+1, closep, maxp)
+            if closep < openp:
+                backtrack(ans, curr+')', openp, closep+1, maxp)
+
+        ans = []
+        openp, closep = 0, 0
+        curr = ''
+        backtrack(ans, curr, openp, closep, n)
+        return ans
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    res = sol.generateParenthesis(3)
+    print(res)
