14d65d0ff8334b3b99db3f09511915be16487707
masa08 <ushimasarug08@gmail.com>
2021-11-15 09:33:46 +0900

    :sparkles: update combination sum

--- leetcode/medium/conbination_sum.py
+++ leetcode/medium/conbination_sum.py
@@ -5,18 +5,21 @@ from typing import List
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
-        n = len(candidates)
 
-        # cur = 現在の配列の要素, cur_sum = 現在の配列の中身の合計
         def dfs(cur, cur_sum, idx):
             if cur_sum > target:
                 return
             if cur_sum == target:
                 ans.append(cur)
                 return
-            for i in range(idx, n):
+            for i in range(idx, len(candidates)):
                 dfs(cur + [candidates[i]], cur_sum + candidates[i], i)
 
         dfs([], 0, 0)
 
         return ans
+
+
+if __name__ == '__main__':
+    target = Solution()
+    print(target.combinationSum([2, 3, 6, 7], 7))
