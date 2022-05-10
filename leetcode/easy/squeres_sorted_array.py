60738e440c8a06f0173be19dc0023c6129e1885a
masa08 <ushimasarug08@gmail.com>
2022-05-10 10:16:30 +0900

    refactor squares sorted array

--- leetcode/easy/squeres_sorted_array.py
+++ leetcode/easy/squeres_sorted_array.py
@@ -10,14 +10,16 @@ class Solution:
         return sorted(result)
 
     def sortedSquares2(self, A: List[int]) -> List[int]:
-        result = [0] * len(A)
+        result = [0 for _ in A]
         left, right = 0, len(A) - 1
-        for index in range(len(A)-1, -1, -1):
+
+        start, stop, step = right, -1, -1
+        for i in range(start, stop, step):
             if abs(A[left]) > abs(A[right]):
-                result[index] = A[left] ** 2
+                result[i] = A[left] ** 2
                 left += 1
             else:
-                result[index] = A[right] ** 2
+                result[i] = A[right] ** 2
                 right -= 1
         return result
 
