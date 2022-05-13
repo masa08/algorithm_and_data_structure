c17fba20136f57853f2ce60a496551ffbbfdfb06
masa08 <ushimasarug08@gmail.com>
2022-05-13 09:27:36 +0900

    refactor frog 2

--- atcoder/edpc/frog2.py
+++ atcoder/edpc/frog2.py
@@ -1,18 +1,26 @@
+# input example
+# 5 3
+# 10 30 40 50 20
+
+
 def main():
     N, K = map(int, input().split())
     h = list(map(int, input().split()))
-    DP = [0]*N
 
-    for i in range(1, N):
+    # init list for dp
+    DP = list(0 for _ in range(N))
+
+    # fix first and secound cost
+    DP[0] = 0
+    DP[1] = abs(h[1]-h[0])
+
+    for i in range(2, N):
         cnt = []
-        # iまで判定、始点はi-kがあればそこから
-        for j in range(max(i-K, 0), i):
-            # 各踏み場からのコストをcntに格納
-            cnt.append(abs(h[i]-h[j])+DP[j])
-        # 最小値を代入
-        DP[i] = min(cnt)
+        for j in range(max(i-K, 0), i): # iまでのコスト判定、始点はi-kがあればそこから
+            cnt.append(abs(h[i]-h[j])+DP[j]) # 各踏み場からのコストをcntに格納
+        DP[i] = min(cnt) # 最小値を代入
 
-    return DP[N-1]
+    return DP[-1]
 
 
 print(main())
