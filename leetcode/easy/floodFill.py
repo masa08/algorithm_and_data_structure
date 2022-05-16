b010b0667b61ea90f119d91ab489b49fcd913ea3
masa08 <ushimasarug08@gmail.com>
2022-05-16 09:30:51 +0900

    refactor flood fill

--- leetcode/easy/floodFill.py
+++ leetcode/easy/floodFill.py
@@ -3,33 +3,42 @@ from typing import List
 
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        neis = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+        if image[sr][sc] == newColor: return image
+
+        # 移動範囲(上、下、右、左)の網羅
+        neis = [[0,1],[0,-1],[1,0],[-1,0]]
         m = len(image)
         n = len(image[0])
-
-        def dfs(row, col, oldColor):
-            for nei in neis:
-                x = row+nei[0]
-                y = col+nei[1]
-                if x < m and x >= 0 and y < n and y >= 0 and image[x][y] == oldColor:
-                    image[x][y] = newColor
-                    dfs(x, y, oldColor)
 
         def bfs(row, col, oldColor):
             q = [[row, col]]
             while q:
                 x_, y_ = q.pop(0)
+                # 起点マスの周辺を全部精査 => qがあればwhileを繰り返す(BFS)
                 for nei in neis:
                     x = x_+nei[0]
                     y = y_+nei[1]
-                    if x < m and x >= 0 and y < n and y >= 0 and image[x][y] == oldColor:
+                    inSquare = True if x < m and x >= 0 and y < n and y >= 0 else False
+                    if inSquare and image[x][y] == oldColor:
                         image[x][y] = newColor
                         q.append([x, y])
 
-        if image[sr][sc] == newColor:
-            return image
-        else:
-            oldColor = image[sr][sc]
-            image[sr][sc] = newColor
-            dfs(sr, sc, oldColor)
-            return image
+        def dfs(row, col, oldColor):
+            #　4-firectionallyの精査
+            for nei in neis:
+                # 1マス移動
+                x = row + nei[0]
+                y = col + nei[1]
+                inSquare = True if x < m and x >= 0 and y < n and y >= 0 else False
+
+                if inSquare and image[x][y] == oldColor:
+                    image[x][y] = newColor
+                    # 移動先から再度DFS
+                    dfs(x,y,oldColor)
+
+        oldColor = image[sr][sc]
+        # 起点マスの色を最初に変更
+        image[sr][sc] = newColor
+        dfs(sr,sc,oldColor)
+        return image
+
