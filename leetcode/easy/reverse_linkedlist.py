df452f3f4b5e94017902871d3bbbfc7f02fa5d27
masa08 <ushimasarug08@gmail.com>
2022-05-18 09:15:49 +0900

    refactor reversed linkedlist

--- leetcode/easy/reverse_linkedlist.py
+++ leetcode/easy/reverse_linkedlist.py
@@ -1,6 +1,6 @@
 # Given the head of a singly linked list, reverse the list, and return the reversed list.
-
-# Definition for singly-linked list.
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -11,14 +11,11 @@ class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         prev = None
         curr = head
-
-        # nexttempを保存する
-        # currのnexttempをprevにする
-        # prevにcurrを保存する
         while curr:
-            nextTemp = curr.next
+            nextListNode = curr.next
             curr.next = prev
             prev = curr
-            curr = nextTemp
+
+            curr = nextListNode
 
         return prev
