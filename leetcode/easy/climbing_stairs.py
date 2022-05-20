f653b644b785da52b2a51348a48ef859e8f34c1a
masa08 <ushimasarug08@gmail.com>
2022-05-20 09:37:26 +0900

    refactor climbing stairs dp way

--- leetcode/easy/climbing_stairs.py
+++ leetcode/easy/climbing_stairs.py
@@ -1,7 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        num1, num2 = 0, 1
+        if n==0: return 0
+        if n==1: return 1
+        if n==2: return 2
 
-        for _ in range(n):
-            num1, num2 = num2, num1+num2
-        return num2
+        dp = [0 for _ in range(n+1)]
+        dp[1]= 1
+        dp[2]=2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
